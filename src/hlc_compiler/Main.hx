27798d72f4071f6b3c8a23376e01a9c606550eb0
FAL <falworks.contact@gmail.com>
2020-06-06 06:37:04 +0900

    separate file for args validation

--- src/hlc_compiler/Main.hx
+++ src/hlc_compiler/Main.hx
@@ -9,9 +9,9 @@ class Main {
 		if (showInstruction(rawArguments)) return;
 
 		try {
-			final arguments = validateArguments(rawArguments);
+			final arguments = ArgumentTools.validateRaw(rawArguments);
 			run(arguments);
-		} catch (e) {
+		} catch (e:Dynamic) {
 			Sys.println("Caught exception:");
 			Sys.println(e);
 			Common.showHint(true, true);
@@ -81,92 +81,6 @@ class Main {
 		contents.push("echo Completed.");
 
 		sys.io.File.saveContent(savePath, contents.join("\n\n") + "\n");
-	}
-
-	/**
-		Validates arguments that were passed in the command line
-	**/
-	static function validateArguments(rawArguments: Array<String>): Arguments {
-		// The last argument should be the hlc-compiler directory
-		final rawArgumentsLength = rawArguments.length - 1;
-		rawArguments = rawArguments.slice(0, rawArgumentsLength);
-		var index = 0;
-
-		inline function hasNext()
-			return index < rawArgumentsLength;
-
-		inline function next()
-			return rawArguments[index++];
-
-		inline function nextOption(option: String) {
-			if (!hasNext())
-				throw 'Missing argument for option: $option';
-			return next();
-		}
-
-		final currentDir = DirectoryRef.current();
-		var srcDir = currentDir;
-		var outFile = currentDir.path.makeFilePath("hlc_bin/main");
-		var hlDir = currentDir;
-		var copyDlls = false;
-		var exFiles: Array<FileRef> = [];
-		var exDlls: Array<FileRef> = [];
-		var exOptions: Array<String> = [];
-		var saveCmdPath: Maybe<FilePath> = Maybe.none();
-		var verbose = false;
-
-		while (hasNext()) {
-			switch (next()) {
-				case "--srcDir":
-					srcDir = DirectoryRef.from(nextOption("--srcDir [directory path]"));
-				case "--outFile":
-					outFile = FilePath.from(nextOption("--outFile [file path]"));
-				case "--hlDir":
-					hlDir = DirectoryRef.from(nextOption("--hlDir [directory path]"));
-				case "--copyDlls":
-					copyDlls = true;
-				case "--exFiles":
-					exFiles = nextOption("--exFiles [comma-separated file paths]").split(",")
-						.map(FileRef.fromStringCallback);
-				case "--exDlls":
-					exDlls = nextOption("--exDlls [comma-separated file paths]").split(",")
-						.map(FileRef.fromStringCallback);
-				case "--saveCmd":
-					saveCmdPath = FilePath.from(nextOption("--saveCmd [file path]"));
-				case "--verbose":
-					verbose = true;
-				case "--version":
-					Common.showVersion(false, true);
-				case otherValue:
-					exOptions.push(otherValue);
-			}
-		}
-
-		if (verbose) {
-			Sys.println('Provided arguments:\n  ${rawArguments.join(" | ")}\n');
-			Sys.println('Validated arguments:');
-			Sys.println('  srcDir:    $srcDir');
-			Sys.println('  outFile:   $outFile');
-			Sys.println('  hlDir:     $hlDir');
-			Sys.println('  copyDlls:  $copyDlls');
-			Sys.println('  exFiles:   $exFiles');
-			Sys.println('  exDlls:    $exDlls');
-			Sys.println('  exOptions: $exOptions');
-			Sys.println('  saveCmd:   ${saveCmdPath.toString()}');
-			Sys.println("");
-		}
-
-		return {
-			srcDir: srcDir,
-			outFile: outFile,
-			hlDir: hlDir,
-			copyDlls: copyDlls,
-			exFiles: exFiles,
-			exDlls: exDlls,
-			exOptions: exOptions,
-			saveCmdPath: saveCmdPath,
-			verbose: verbose
-		};
 	}
 
 	/**
