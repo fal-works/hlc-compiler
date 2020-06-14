b7d948b4f0736f25a88d03866990ef2ae574e6b7
FAL <falworks.contact@gmail.com>
2020-06-14 19:46:41 +0900

    refactor

--- src/hlc_compiler/LibraryTools.hx
+++ src/hlc_compiler/LibraryTools.hx
@@ -16,57 +16,49 @@ class LibraryTools {
 		final hlcJsonData: HlcJsonData = Json.parse(hlcJsonFile.getContent());
 		final systemType = Environment.systemType;
 
-		for (lib in hlcJsonData.libs) {
-			switch lib {
-				case "std":
-					switch systemType {
-						case Windows:
-							libs.push(Static(Name("libhl"))); // "-lhl" seems to hit another file
-							libs.push(Shared(hlLibDir.findFile("libhl.dll")));
-						case Mac:
-							libs.push(Static(Name("hl")));
-							// Seems it's not required at runtime
-					}
-				case "openal":
-					switch systemType {
-						case Windows:
-							libs.push(Static(Name("openal")));
-							libs.push(Shared(hlLibDir.findFile("openal.hdll")));
-							libs.push(Shared(hlLibDir.findFile("OpenAL32.dll")));
-						case Mac:
-							// TODO: test
-							libs.push(Static(Name("openal")));
-							libs.push(Shared(hlLibDir.findFile("openal.hdll")));
-					}
-				case "sdl":
-					switch systemType {
-						case Windows:
-							libs.push(Static(Name("sdl2")));
-							libs.push(Shared(hlLibDir.findFile("sdl.hdll")));
-							libs.push(Shared(hlLibDir.findFile("SDL2.dll")));
-						case Mac:
-							// TODO: test
-							libs.push(Static(Name("sdl2")));
-							libs.push(Shared(hlLibDir.findFile("sdl.hdll")));
-					}
-				default:
-					final hdllPath = hlLibDirPath.makeFilePath('$lib.hdll');
-					switch systemType {
-						case Windows:
-							// final libPath = hlLibDirPath.makeFilePath('$lib.lib'); // Don't know why but *.lib files don't work
-							final dllPath = hlLibDirPath.makeFilePath('$lib.dll');
-							final file = FileRef.from(hdllPath.or(dllPath));
-							libs.push(Static(File(file)));
-							libs.push(Shared(file));
-						case Mac:
-							// TODO: test
-							// final aPath = hlLibDirPath.makeFilePath('$lib.a').or(hlLibDirPath.makeFilePath('lib$lib.a'));
-							final dylibPath = hlLibDirPath.makeFilePath('$lib.dylib')
-								.or(hlLibDirPath.makeFilePath('lib$lib.dylib'));
-							libs.push(Static(Name(lib)));
-							libs.push(Shared(FileRef.from(hdllPath.or(dylibPath))));
-					}
-			};
+		switch systemType {
+			case Windows:
+				for (lib in hlcJsonData.libs) switch lib {
+					case "std":
+						libs.push(Static(Name("libhl"))); // "-lhl" seems to hit another file
+						libs.push(Shared(hlLibDir.findFile("libhl.dll")));
+					case "openal":
+						libs.push(Static(Name("openal")));
+						libs.push(Shared(hlLibDir.findFile("openal.hdll")));
+						libs.push(Shared(hlLibDir.findFile("OpenAL32.dll")));
+					case "sdl":
+						libs.push(Static(Name("sdl2")));
+						libs.push(Shared(hlLibDir.findFile("sdl.hdll")));
+						libs.push(Shared(hlLibDir.findFile("SDL2.dll")));
+					default:
+						final hdllPath = hlLibDirPath.makeFilePath('$lib.hdll');
+						// final libPath = hlLibDirPath.makeFilePath('$lib.lib'); // Don't know why but *.lib files don't work
+						final dllPath = hlLibDirPath.makeFilePath('$lib.dll');
+						final file = FileRef.from(hdllPath.or(dllPath));
+						libs.push(Static(File(file)));
+						libs.push(Shared(file));
+				};
+			case Mac:
+				for (lib in hlcJsonData.libs) switch lib {
+					case "std":
+						libs.push(Static(Name("hl"))); // Seems dynamic link is not required. Just -lhl
+					case "openal":
+						// TODO: test
+						libs.push(Static(Name("openal")));
+						libs.push(Shared(hlLibDir.findFile("openal.hdll")));
+					case "sdl":
+						// TODO: test
+						libs.push(Static(Name("sdl2")));
+						libs.push(Shared(hlLibDir.findFile("sdl.hdll")));
+					default:
+						final hdllPath = hlLibDirPath.makeFilePath('$lib.hdll');
+						// TODO: test
+						// final aPath = hlLibDirPath.makeFilePath('$lib.a').or(hlLibDirPath.makeFilePath('lib$lib.a'));
+						final dylibPath = hlLibDirPath.makeFilePath('$lib.dylib')
+							.or(hlLibDirPath.makeFilePath('lib$lib.dylib'));
+						libs.push(Static(Name(lib)));
+						libs.push(Shared(FileRef.from(hdllPath.or(dylibPath))));
+				};
 		}
 
 		return libs;
