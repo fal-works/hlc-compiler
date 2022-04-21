3148137830d50c4346c17cf3729ee1f782cba630
FAL <falworks.contact@gmail.com>
2022-04-22 00:17:44 +0900

    refactor: Main

---
+++ src/hlc_compiler/LibrarySearcher.hx
@@ -0,0 +1,88 @@
+package hlc_compiler;
+
+import hlc_compiler.types.HlcJson;
+import hlc_compiler.types.Library;
+import hlc_compiler.types.LibraryList;
+
+/**
+	@return Library files required by `hlcJsonFile`.
+**/
+function getRequiredLibraries(
+	hlcJsonFile: FileRef,
+	hlLibDir: DirectoryRef
+): LibraryList {
+	inline function findHdll(name: String): FileRef
+		return hlLibDir.findFile('$name.hdll');
+
+	inline function getHdllPath(name: String): FilePath
+		return hlLibDir.path.makeFilePath('$name.hdll');
+
+	inline function findDll(name: String): FileRef
+		return hlLibDir.findFile('$name.dll');
+
+	final libs: Array<Library> = [];
+	final hlcJson = HlcJson.parse(hlcJsonFile);
+
+	switch Environment.systemType {
+		case Windows:
+			for (lib in hlcJson.libs) switch lib {
+				case "std":
+					libs.push(Static(Name("libhl"))); // "-lhl" seems to hit another file
+					libs.push(Shared(findDll("libhl")));
+				case "openal":
+					libs.push(StaticShared(findHdll("openal"), null));
+					libs.push(Shared(findDll("OpenAL32")));
+				case "sdl":
+					libs.push(StaticShared(findHdll("sdl"), null));
+					libs.push(Shared(findDll("SDL2")));
+				case "fmt" | "directx" | "ui" | "uv" | "ssl" | "mysql" | "sqlite":
+					libs.push(StaticShared(findHdll(lib), null));
+				default:
+					warn('Unknown library: $lib');
+					final hdllPath = getHdllPath(lib);
+					if (hdllPath.exists()) {
+						libs.push(StaticShared(hdllPath.find(), null));
+					} else {
+						final libPath = hlLibDir.makeFilePath('$lib.lib')
+							.or(hlLibDir.makeFilePath('lib$lib.lib'));
+						final dllPath = hlLibDir.makeFilePath('$lib.dll')
+							.or(hlLibDir.makeFilePath('lib$lib.dll'));
+						if (libPath.or(dllPath).exists())
+							libs.push(Static(Name(lib)));
+						if (dllPath.exists())
+							libs.push(Shared(dllPath.find()));
+					}
+			};
+		case Mac:
+			for (lib in hlcJson.libs) switch lib {
+				case "std":
+					libs.push(Static(Name("hl")));
+				case "fmt" | "openal" | "ui" | "mysql" | "ssl":
+					libs.push(Static(File(findHdll(lib))));
+				case "sdl":
+					libs.push(Static(Name("sdl2")));
+					libs.push(Static(File(findHdll(lib))));
+				case "uv":
+					libs.push(Static(Name(lib)));
+					libs.push(Static(File(findHdll(lib))));
+				case "sqlite":
+					final hdllPath = getHdllPath(lib);
+					if (!hdllPath.exists()) {
+						var msg = "File not found: sqlite.hdll";
+						msg += "\n  See also: https://github.com/HaxeFoundation/hashlink/pull/323";
+						throw error(msg);
+					}
+					libs.push(Static(File(hdllPath.find())));
+				default:
+					warn('Unknown library: $lib');
+					final hdllPath = getHdllPath(lib);
+					if (hdllPath.exists()) {
+						libs.push(Static(File(hdllPath.find())));
+					} else {
+						libs.push(Static(Name(lib)));
+					}
+			};
+	}
+
+	return libs;
+}
--- src/hlc_compiler/Main.hx
+++ src/hlc_compiler/Main.hx
@@ -1,8 +1,6 @@
 package hlc_compiler;
 
 import hlc_compiler.OptionsParser.parseOptions;
-import hlc_compiler.save.SaveCommandTools;
-import hlc_compiler.types.Arguments;
 
 class Main {
 	/**
@@ -46,97 +44,11 @@ class Main {
 	**/
 	public static function tryProcessArguments(args: Array<RawArgument>): Void {
 		try {
-			parseOptions(args).may(run);
+			parseOptions(args).may(Runner.run);
 		} catch (e) {
 			Sys.println('Caught exception:\n$e');
 			Common.showHint(true, true);
 			Sys.exit(1);
 		}
 	}
-
-	/**
-		Runs the main process of hlc-compiler according to `arguments`.
-		- Compiles HL/C into executable.
-		- (If specified) Copies runtime files.
-		- (If specified) Saves command lines.
-	**/
-	public static function run(arguments: Arguments): Void {
-		final verbose = arguments.verbose;
-
-		final prepared = prepareRun(arguments);
-		final compileCommand = prepared.compileCommand;
-		final copyList = prepared.copyList;
-
-		final outDir = arguments.outDir.findOrCreate(); // Prepare dir before compiling
-
-		Sys.println("Compiling...");
-		final errorLevel = compileCommand.run(verbose);
-
-		if (errorLevel != 0) {
-			final msg = if (verbose) {
-				"Compilation command failed."; // Command already printed if verbose
-			} else {
-				'Compilation command failed:\n${compileCommand.quote(Cli.current)}';
-			}
-			throw error(msg);
-		}
-
-		if (0 < copyList.length) {
-			Sys.println("Copying runtime files...");
-			if (verbose) Sys.println('${copyList.getNames()} => ${outDir.path}');
-			copyList.copyTo(outDir.path);
-		}
-
-		Sys.println("Completed.");
-
-		final saveCmdPath = arguments.saveCmdPath;
-		if (saveCmdPath.isSome()) {
-			final savePath = saveCmdPath.unwrap();
-			final savedPath = switch Environment.systemType {
-				case Windows:
-					SaveCommandTools.saveCommandBat(
-						savePath,
-						outDir,
-						compileCommand,
-						copyList,
-						arguments.relative
-					);
-				case Mac:
-					SaveCommandTools.saveCommandShell(
-						savePath,
-						outDir,
-						compileCommand,
-						copyList,
-						arguments.relative
-					);
-			}
-			Sys.println('Saved command: $savedPath');
-		}
-	}
-
-	/**
-		Prepares for `run()`.
-	**/
-	static function prepareRun(arguments: Arguments): PreparedData {
-		final hlLibs = LibraryTools.getRequiredLibraries(
-			arguments.hlcJsonFile,
-			arguments.hlLibDir
-		);
-		final hlLibsToCopy = hlLibs.filterShared().map(FileOrDirectoryRef.fromFileCallback);
-
-		return {
-			compileCommand: GccCommandBuilder.build(
-				arguments,
-				hlLibs.filterStatic(),
-				Cli.current
-			),
-			copyList: if (!arguments.copyRuntimeFiles) [] else
-				hlLibsToCopy.concat(arguments.runtime)
-		}
-	}
 }
-
-private typedef PreparedData = {
-	final compileCommand: CommandLine;
-	final copyList: FileOrDirectoryList;
-};
