fde926ee39627af1f277f1728843267bea5c9833
FAL <falworks.contact@gmail.com>
2022-04-23 01:14:10 +0900

    test: fix runOutput() on Unix

--- test/TestConfig.hx
+++ test/TestConfig.hx
@@ -26,7 +26,7 @@ function clearOutput() {
 function runOutput() {
 	Sys.println("Run the compiled executable...");
 	switch Cli.current.type {
-		case Unix: Sys.command("open", ["out/bin/main"]);
+		case Unix: Sys.command("out/bin/main");
 		case Dos: Sys.command("call", ["out\\bin\\main"]);
 	}
 }
